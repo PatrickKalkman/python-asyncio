a05932f8db599654400d515d3d112ceab604df12
Patrick Kalkman <patrick@simpletechture.nl>
2023-04-28 14:50:03 +0200

    Added client and chat server example

---
+++ async_chat_client.py
@@ -0,0 +1,30 @@
+import asyncio
+
+async def send_message(writer):
+    while True:
+        message = input("Enter message: ")
+        writer.write(message.encode())
+        await writer.drain()
+
+async def receive_message(reader):
+    while True:
+        data = await reader.read(100)
+        if not data:
+            print("Connection closed.")
+            break
+        print(data.decode())
+
+async def main():
+    reader, writer = await asyncio.open_connection("localhost", 12345)
+
+    try:
+        await asyncio.gather(send_message(writer), receive_message(reader))
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Client stopped.")
---
+++ async_chat_server.py
@@ -0,0 +1,29 @@
+import asyncio
+
+async def handle_client(reader, writer):
+    addr = writer.get_extra_info("peername")
+    print(f"New connection from {addr}")
+
+    while True:
+        message = await reader.read(100)
+        if not message:
+            print(f"Connection closed with {addr}")
+            break
+
+        print(f"Received message from {addr}: {message.decode()}")
+        writer.write(f"Echo: {message.decode()}".encode())
+        await writer.drain()
+
+async def main():
+    server = await asyncio.start_server(handle_client, "localhost", 12345)
+    addr = server.sockets[0].getsockname()
+    print(f"Serving on {addr}")
+
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Server stopped.")
